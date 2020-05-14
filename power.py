4f8257024548e4d57fb0ebddc9e5bb9e873c7f8b
Philip Garner <philtgarner@gmail.com>
2020-05-14 20:17:48 +0100

    Added LED changing in response to power readings
    Power readings averaged over a time period specified in the config file

--- power.py
+++ power.py
@@ -1,3 +1,6 @@
+#!/home/pi/.local/share/virtualenvs/power-led-EXGU0oXn/bin/python3
+
+import os
 import pygatt
 import yaml
 import logging
@@ -5,6 +8,35 @@ import time
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+import board
+import neopixel
+import RPi.GPIO as GPIO
+
+# The pin that the NeoPixel ring is connected to
+pixel_pin = board.D18
+
+# The number of LEDs in the NeoPixel ring
+num_pixels = 12
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+GPIO.setmode(GPIO.BCM)
+
+# Power zone colours
+grey = (100, 100, 100)
+blue = (0, 0, 100)
+green = (0, 100, 0)
+yellow = (100, 100, 0)
+orange = (120, 65, 0)
+red = (100, 0, 0)
+off = (0, 0, 0)
+
+# Keep track of the current colour
+current_colour = off
+
+# Set up the pixel ring
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
 # Default FTP
 ftp = 200
@@ -48,34 +80,40 @@ def handle_data(handle, value):
     average_power = power_readings['power'].mean()
     led_colour = get_colour(average_power)
 
-    # TODO Light the LED in the desired colour
-
     print('Average power: %d' % average_power)
-    print('Power colour: %s' % led_colour)
+
+    change_colour(led_colour)
 
 
-# TODO Change this from a sample method to something that returns actual colours
+def change_colour(new_colour):
+    global current_colour
+    global pixels
+    interval = 0.05
+
+    if new_colour != current_colour:
+        for pixel in range(num_pixels):
+            pixels[pixel] = new_colour
+            pixels.show()
+            time.sleep(interval)
+
+        current_colour = new_colour
+
+
 def get_colour(power):
     percentage = power / ftp
 
-    if percentage <= 59:
-        # grey
-        return 'grey'
-    elif percentage <= 75:
-        # blue
-        return 'blue'
-    elif percentage <= 89:
-        # green
-        return 'green'
-    elif percentage <= 104:
-        # yellow
-        return 'yellow'
-    elif percentage <= 118:
-        # orange
-        return 'orange'
+    if percentage <= 0.59:
+        return grey
+    elif percentage <= 0.75:
+        return blue
+    elif percentage <= 0.89:
+        return green
+    elif percentage <= 1.04:
+        return yellow
+    elif percentage <= 1.18:
+        return orange
     else:
-        # red
-        return 'red'
+        return red
 
 
 if __name__ == '__main__':
@@ -83,8 +121,13 @@ if __name__ == '__main__':
     logging.basicConfig()
     logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
+    change_colour(blue)
+    time.sleep(3)
+    change_colour(off)
+
     # Get the items from the config file
-    config = yaml.safe_load(open('config.yml'))
+    config_file_location = os.path.join(os.path.dirname(__file__), 'config.yml')
+    config = yaml.safe_load(open(config_file_location))
     device_id = config['bluetooth-device']
     ftp = config['ftp']
     power_average_duration = config['power-average-duration']
@@ -98,12 +141,19 @@ if __name__ == '__main__':
         # Connect to the power meter
         device = adapter.connect(device_id, address_type=address_type)
 
+        change_colour(green)
+        time.sleep(3)
+        change_colour(off)
+
         # Subscribe to the Cycling Power Measurement characteristic
         device.subscribe(characteristic, callback=handle_data)
 
-        # Sleep for twenty seconds so we can receive some data
-        # TODO We probably need to put something like a while(True) in here
-        # TODO But we'll need to know how to handle stopping the adapter
-        time.sleep(20)
+        # Sleep while we receive events
+        while(True):
+            time.sleep(1000)
+    except:
+        change_colour(red)
+        time.sleep(3)
+        change_colour(off)
     finally:
         adapter.stop()
